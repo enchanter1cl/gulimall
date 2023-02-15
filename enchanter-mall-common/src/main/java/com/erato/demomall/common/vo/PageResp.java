71b58add0b2dae22de029c8baa20c362291b5e76
zhangYuan <zhangyuan@authing.cn>
2023-02-15 11:51:33 +0800

    feat: common vo for paging

---
+++ enchanter-mall-common/src/main/java/com/erato/demomall/common/vo/PageResp.java
@@ -0,0 +1,22 @@
+package com.erato.demomall.common.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author ZhangYuan
+ * @date 2023/2/13
+ */
+
+@Data
+public class PageResp<T> {
+    
+    private Long total;
+    
+    private Integer curPage;
+    
+    private Integer pageSize;
+    
+    private List<T> list;
+}
